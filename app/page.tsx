0d4878fa393bcf86cc8c075ae4377faf4a51a6d1
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-11-28 12:40:19 +0530

    about

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Container from "@/components/common/Container";
 import Hero from "@/components/landing/Hero";
 import Experience from "@/components/landing/Experience";
+import About from "@/components/landing/About";
 
 export default function Home() {
   return (
      <Container className="min-h-screen py-16">
       <Hero/>
       <Experience/>
+      <About/>
      </Container>
   );
 }
